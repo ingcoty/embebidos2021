dea84215c50c83f67e92186c20853a680094a721
carlos cabas <ing.carloscabas@gmail.com>
2021-03-23 17:16:54 -0500

    funciones walkList y free_memory agregadas

--- estructuras.c
+++ estructuras.c
@@ -28,11 +28,58 @@ void insertar(Nodo* nodo){
     }
 }
 
+void walkList(){
+    Nodo* i = header;
+    if(i == NULL){
+        printf("Lista Vacia\r\n");        
+        return;
+    } 
+    while(i != NULL){
+        printf("%d->", i->dato);
+        i = i->ptrNext;
+    } 
+    printf("\r\n");  
+}
+
+void free_memory(){
+    Nodo* i = header;
+    if(i == NULL){
+        printf("Lista Vacía\r\n");
+        return;
+    }
+    while(i != NULL){
+        header = i->ptrNext;
+        free(i);
+        i = header;
+    }
+    header = NULL;
+    footer = NULL;
+    printf("Memoria Liberada\r\n");
+}
 
 int main(){
 
     Nodo* nodo = new();
     nodo->dato = 5;
+    insertar(nodo);
+
+    Nodo* nodo2 = new();
+    nodo2->dato = 10;
+    insertar(nodo2);
+    
+    Nodo* nodo3 = new();
+    nodo3->dato = 15;
+    insertar(nodo3);
+
+    Nodo* nodo4 = new();
+    nodo4->dato = 20;
+    insertar(nodo4);
+
+    walkList();
+
+    free_memory();
+
+    walkList();
        
     return(0);
 }
